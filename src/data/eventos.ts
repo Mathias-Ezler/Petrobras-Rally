3827127186c940dfdb95f78e2f9312f8261cfc5b
Mathias Enzler <mathiasenzlerhp@gmail.com>
2025-02-11 18:43:08 -0300

    Commit final-(nose que poner)

--- src/data/eventos.ts
+++ src/data/eventos.ts
@@ -23,46 +23,50 @@ export const eventos: Evento[] = [
     ubicacion: "Itapúa",
     resumen: "El legendario rally que abre la temporada del rally paraguayo en los caminos de Nueva Alborda, Trinidad y Hoenau esta de vuelta.",
     descripcion: "El Rally de las colonias es uno de los eventos más antiguos y prestigiosos en el Paraguay. Conocido por impredecibles carreteras desafiantes, este rally pone a prueba la habilidad de los pilotos. Los competidores navegan por las estrechas y sinuosas carreteras, enfrentándose a cambios repentinos en la superficie y la visibilidad.",
-    rutaMapaUrl: "/lascolonias.png",
+    rutaMapaUrl: "/eventos/img1.webp",
     galeriaFotos: [
-      "/foto2.png",
-      "/fondo.png",
+      "/eventos/img1.webp",
+      "/eventos/img2.webp",
+      "/eventos/img3.webp",
+      "/eventos/img4.webp",
+      "/eventos/img5.webp",
+      "/eventos/img6.webp"
     ],
     detallesTecnicos: "Distancia total: 324.39 km, Etapas cronometradas: 17, Superficie: Asfalto/Nieve",
     patrocinadores: ["Pirelli", "Red Bull", "Citroën"]
   },
- {
-  "id": 2,
-  "slug": "rally-chaco-2025",
-  "nombre": "Rally del Chaco",
-  "fecha": "03-06 Febrero, 2025",
-  "ubicacion": "Región Occidental, Paraguay",
-  "resumen": "El Rally del Chaco vuelve más fuerte que nunca para desafiar a los pilotos de todo Paraguay, con sus caminos de arena, tierra y el calor extremo característico de la región.",
-  "descripcion": "El Rally del Chaco es una de las competencias más icónicas de Paraguay y una de las más exigentes del calendario nacional. Los pilotos enfrentan terrenos variados que incluyen arena, tierra y caminos con temperaturas extremas. Este rally es conocido por su resistencia y la habilidad requerida para dominar los duros tramos del Chaco paraguayo. La estrategia, la preparación técnica y la resistencia física y mental son claves para el éxito.",
-  "rutaMapaUrl": "/foto2.png",
-  "galeriaFotos": [
-    "/fondo.png",
-    "/foto2.png",
-  ],
-  "detallesTecnicos": "Distancia total: 1,200 km, Etapas cronometradas: 25, Superficie: Arena/Tierra",
-  "patrocinadores": ["Petropar", "Banco Nacional de Fomento", "Cervepar", "Ueno Bank"]
-},
-{
-  "id": 4,
-  "slug": "rally-itapua-2025",
-  "nombre": "Rally Trans Itapúa",
-  "fecha": "22-24 Marzo, 2025",
-  "ubicacion": "Encarnación, Itapúa, Paraguay",
-  "resumen": "El Rally Trans Itapúa, uno de los eventos más esperados del automovilismo nacional, regresa con etapas llenas de adrenalina y paisajes espectaculares de la región sur del Paraguay.",
-  "descripcion": "El Rally Trans Itapúa es reconocido como una de las competencias más emocionantes del calendario automovilístico paraguayo. Con tramos que cruzan colinas, caminos rurales y áreas boscosas, este rally ofrece un desafío único tanto para pilotos como para sus equipos. Este evento es también parte del Campeonato Nacional y atrae a miles de espectadores, quienes disfrutan de la velocidad y el espectáculo en medio de los hermosos paisajes de Itapúa.",
-  "rutaMapaUrl": "/fondo.png",
-  "galeriaFotos": [
-    "/fondo.png",
-    "/foto2.png"
-  ],
-  "detallesTecnicos": "Distancia total: 900 km, Etapas cronometradas: 18, Superficie: Tierra/Grava",
-  "patrocinadores": ["Petrobras", "Cervepar", "ABC Color", "Turismo SENATUR"]
-}
-
-
+  {
+    id: 2,
+    slug: "rally-chaco-2025",
+    nombre: "Rally del Chaco",
+    fecha: "03-06 Febrero, 2025",
+    ubicacion: "Región Occidental, Paraguay",
+    resumen: "El Rally del Chaco vuelve más fuerte que nunca para desafiar a los pilotos de todo Paraguay, con sus caminos de arena, tierra y el calor extremo característico de la región.",
+    descripcion: "El Rally del Chaco es una de las competencias más icónicas de Paraguay y una de las más exigentes del calendario nacional. Los pilotos enfrentan terrenos variados que incluyen arena, tierra y caminos con temperaturas extremas. Este rally es conocido por su resistencia y la habilidad requerida para dominar los duros tramos del Chaco paraguayo. La estrategia, la preparación técnica y la resistencia física y mental son claves para el éxito.",
+    rutaMapaUrl: "/eventos/img2.webp",
+    galeriaFotos: [
+      "/eventos/img3.webp",
+      "/eventos/img4.webp",
+      "/eventos/img5.webp",
+      "/eventos/img6.webp"
+    ],
+    detallesTecnicos: "Distancia total: 1,200 km, Etapas cronometradas: 25, Superficie: Arena/Tierra",
+    patrocinadores: ["Petropar", "Banco Nacional de Fomento", "Cervepar", "Ueno Bank"]
+  },
+  {
+    id: 4,
+    slug: "rally-itapua-2025",
+    nombre: "Rally Trans Itapúa",
+    fecha: "22-24 Marzo, 2025",
+    ubicacion: "Encarnación, Itapúa, Paraguay",
+    resumen: "El Rally Trans Itapúa, uno de los eventos más esperados del automovilismo nacional, regresa con etapas llenas de adrenalina y paisajes espectaculares de la región sur del Paraguay.",
+    descripcion: "El Rally Trans Itapúa es reconocido como una de las competencias más emocionantes del calendario automovilístico paraguayo. Con tramos que cruzan colinas, caminos rurales y áreas boscosas, este rally ofrece un desafío único tanto para pilotos como para sus equipos. Este evento es también parte del Campeonato Nacional y atrae a miles de espectadores, quienes disfrutan de la velocidad y el espectáculo en medio de los hermosos paisajes de Itapúa.",
+    rutaMapaUrl: "/eventos/img5.webp",
+    galeriaFotos: [
+      "/eventos/img2.webp",
+      "/eventos/img6.webp"
+    ],
+    detallesTecnicos: "Distancia total: 900 km, Etapas cronometradas: 18, Superficie: Tierra/Grava",
+    patrocinadores: ["Petrobras", "Cervepar", "ABC Color", "Turismo SENATUR"]
+  }
 ];
--- src/data/pilotos.ts
+++ src/data/pilotos.ts
@@ -21,7 +21,7 @@ export const pilotos: Piloto[] = [
         edad: 32,
         experiencia: "10 años",
         victorias: 15,
-        imagen: "/saba.png",
+        imagen: "/pilotos/saba.webp",
         nacionalidad: "Paraguay",
         descripcionCorta: "Campeón nacional de rally 2023",
         descripcionCompleta: "Carlos Santos es uno de los pilotos más experimentados del equipo Petrobras Rally. Con una década de experiencia en competencias nacionales e internacionales, ha demostrado su excelencia en las pistas más desafiantes de Sudamérica.",
@@ -38,7 +38,7 @@ export const pilotos: Piloto[] = [
         edad: 28,
         experiencia: "7 años",
         victorias: 12,
-        imagen: "/diego.png",
+        imagen: "/pilotos/diego.webp",
         nacionalidad: "Paraguay",
         descripcionCorta: "Navegante experto en terrenos difíciles",
         descripcionCompleta: "Ana Silva se ha convertido en una de las copilotos más respetadas en el circuito de rally brasileño. Su precisión en la navegación y capacidad para mantener la calma en situaciones extremas la han convertido en un elemento crucial del equipo.",
@@ -47,5 +47,124 @@ export const pilotos: Piloto[] = [
             "12 victorias en competencias nacionales",
             "Reconocimiento especial por navegación en Rally Amazonas"
         ]
-    }
+    },
+    {
+        id: 3,
+        nombre: "Alejandro Galanti",
+        rol: "piloto",
+        edad: 28,
+        experiencia: "7 años",
+        victorias: 12,
+        imagen: "/pilotos/alejandro-galanti.webp",
+        nacionalidad: "Paraguay",
+        descripcionCorta: "Navegante experto en terrenos difíciles",
+        descripcionCompleta: "Ana Silva se ha convertido en una de las copilotos más respetadas en el circuito de rally brasileño. Su precisión en la navegación y capacidad para mantener la calma en situaciones extremas la han convertido en un elemento crucial del equipo.",
+        logros: [
+            "Mejor copiloto del año 2023",
+            "12 victorias en competencias nacionales",
+            "Reconocimiento especial por navegación en Rally Amazonas"
+        ]
+    },
+    {
+        id: 4,
+        nombre: "Miguel Zaldívar",
+        rol: "piloto",
+        edad: 28,
+        experiencia: "7 años",
+        victorias: 12,
+        imagen: "/pilotos/zaldivar-miguel.webp",
+        nacionalidad: "Paraguay",
+        descripcionCorta: "Navegante experto en terrenos difíciles",
+        descripcionCompleta: "Ana Silva se ha convertido en una de las copilotos más respetadas en el circuito de rally brasileño. Su precisión en la navegación y capacidad para mantener la calma en situaciones extremas la han convertido en un elemento crucial del equipo.",
+        logros: [
+            "Mejor copiloto del año 2023",
+            "12 victorias en competencias nacionales",
+            "Reconocimiento especial por navegación en Rally Amazonas"
+        ]
+    },
+    {
+        id: 5,
+        nombre: "Fabrizio Zaldívar",
+        rol: "piloto",
+        edad: 28,
+        experiencia: "7 años",
+        victorias: 12,
+        imagen: "/pilotos/fabrizio-zaldivar.webp",
+        nacionalidad: "Paraguay",
+        descripcionCorta: "Navegante experto en terrenos difíciles",
+        descripcionCompleta: "Ana Silva se ha convertido en una de las copilotos más respetadas en el circuito de rally brasileño. Su precisión en la navegación y capacidad para mantener la calma en situaciones extremas la han convertido en un elemento crucial del equipo.",
+        logros: [
+            "Mejor copiloto del año 2023",
+            "12 victorias en competencias nacionales",
+            "Reconocimiento especial por navegación en Rally Amazonas"
+        ]
+    },
+    {
+        id: 6,
+        nombre: "Fabricio Galanti",
+        rol: "piloto",
+        edad: 28,
+        experiencia: "7 años",
+        victorias: 12,
+        imagen: "/pilotos/fabi-galanti.webp",
+        nacionalidad: "Paraguay",
+        descripcionCorta: "Navegante experto en terrenos difíciles",
+        descripcionCompleta: "Ana Silva se ha convertido en una de las copilotos más respetadas en el circuito de rally brasileño. Su precisión en la navegación y capacidad para mantener la calma en situaciones extremas la han convertido en un elemento crucial del equipo.",
+        logros: [
+            "Mejor copiloto del año 2023",
+            "12 victorias en competencias nacionales",
+            "Reconocimiento especial por navegación en Rally Amazonas"
+        ]
+    },
+    {
+        id: 7,
+        nombre: "Juan Martín Masi",
+        rol: "piloto",
+        edad: 28,
+        experiencia: "7 años",
+        victorias: 12,
+        imagen: "/pilotos/juan.webp",
+        nacionalidad: "Paraguay",
+        descripcionCorta: "Navegante experto en terrenos difíciles",
+        descripcionCompleta: "Ana Silva se ha convertido en una de las copilotos más respetadas en el circuito de rally brasileño. Su precisión en la navegación y capacidad para mantener la calma en situaciones extremas la han convertido en un elemento crucial del equipo.",
+        logros: [
+            "Mejor copiloto del año 2023",
+            "12 victorias en competencias nacionales",
+            "Reconocimiento especial por navegación en Rally Amazonas"
+        ]
+    },
+    {
+        id: 8,
+        nombre: "Augusto Bestard",
+        rol: "piloto",
+        edad: 28,
+        experiencia: "7 años",
+        victorias: 12,
+        imagen: "/pilotos/augusto.webp",
+        nacionalidad: "Paraguay",
+        descripcionCorta: "Navegante experto en terrenos difíciles",
+        descripcionCompleta: "Ana Silva se ha convertido en una de las copilotos más respetadas en el circuito de rally brasileño. Su precisión en la navegación y capacidad para mantener la calma en situaciones extremas la han convertido en un elemento crucial del equipo.",
+        logros: [
+            "Mejor copiloto del año 2023",
+            "12 victorias en competencias nacionales",
+            "Reconocimiento especial por navegación en Rally Amazonas"
+        ]
+    },
+    {
+        id: 9,
+        nombre: "H Zaldívar",
+        rol: "piloto",
+        edad: 28,
+        experiencia: "7 años",
+        victorias: 12,
+        imagen: "/pilotos/zaldivarh.webp",
+        nacionalidad: "paraguay",
+        descripcionCorta: "Navegante experto en terrenos difíciles",
+        descripcionCompleta: "Ana Silva se ha convertido en una de las copilotos más respetadas en el circuito de rally brasileño. Su precisión en la navegación y capacidad para mantener la calma en situaciones extremas la han convertido en un elemento crucial del equipo.",
+        logros: [
+            "Mejor copiloto del año 2023",
+            "12 victorias en competencias nacionales",
+            "Reconocimiento especial por navegación en Rally Amazonas"
+        ]
+    },
 ]
